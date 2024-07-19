1ab2dd720f53f5636867d30480adec118d26da27
Tanzeela-fatima <tanzeelafatima261@gmail.com>
2024-07-19 18:53:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let grams=prompt("The rate of 1kg chicken is 500rs, how many chicken do you want in grams? & i'll convert it into rupees");
+
+
+function newprice(){
+     grams=prompt("The rate of 1kg chicken is 500rs, how many chicken do you want in grams? & i'll convert it into rupees");
+     let total_price = grams/kilogram1*totalprice;
+     document.write("\n"+total_price);
+     
+}
+
+let totalprice= 500 ;
+let kilogram1= 1000;
+let total_price = grams/kilogram1*totalprice;
+     document.write(total_price);
+     console.log(total_price);
